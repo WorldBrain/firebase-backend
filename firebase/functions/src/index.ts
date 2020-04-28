a32822779884577f261fb971b53b4937a441c0e2
Charris <c@harrischris.com>
2020-04-28 14:23:47 +0200

    Update minor firebase versions to fix type issues

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,6 +1,5 @@
 import * as admin from 'firebase-admin'
 import { runningInEmulator, emulatedConfig } from './constants';
-
 admin.initializeApp((runningInEmulator) ? emulatedConfig : undefined);
 
 export { getLoginToken } from './auth'
