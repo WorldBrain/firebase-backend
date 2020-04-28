ee62ee548993220bc0d62587d7af845eaf07fe6f
Charris <c@harrischris.com>
2020-04-28 12:57:37 +0200

    Add new optional types for subscription status in user claims

--- ts/subscriptions/types.ts
+++ ts/subscriptions/types.ts
@@ -4,16 +4,26 @@ export interface SubscriptionsService {
     getManageLink(options?: SubscriptionCheckoutOptions): Promise<string>
 }
 
+// Taken from: https://apidocs.chargebee.com/docs/api/subscriptions#subscription_status
+export type SubscriptionStatus = 'in_trial' | 'future' | 'active' | 'non_renewing' | 'paused' | 'cancelled' | null
+
 export interface Claims {
+    subscriptionStatus?: SubscriptionStatus,
+    subscriptionExpiry?: number| null, // Epoch in seconds
     subscriptions: SubscriptionMap
     features: FeatureMap
     lastSubscribed: number | null
 }
 export type SubscriptionMap = {
-    [key in UserPlan]?: { expiry: number }
+    [key in UserPlan]?: {
+        expiry: number, // Epoch in seconds
+        status?: SubscriptionStatus,
+    }
 }
 export type FeatureMap = {
-    [key in UserFeature]?: { expiry: number }
+    [key in UserFeature]?: {
+        expiry: number, // Epoch in seconds
+    }
 }
 export type UserFeature = 'backup' | 'sync'
 export type UserPlan =
