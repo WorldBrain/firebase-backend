bc77db84fa7518f293634b276f15ee6795ee0dce
Charris <c@harrischris.com>
2020-05-04 09:55:45 +0200

    Uninstall analytics function

---
+++ firebase/functions/src/analytics/index.ts
@@ -0,0 +1,21 @@
+import * as functions from 'firebase-functions'
+import { Request} from 'firebase-functions/lib/providers/https'
+import * as express from "express";
+
+const appKey = functions.config().countly['app_key']
+
+export const uninstall = functions.https.onRequest(async (req: Request, resp: express.Response) => {
+
+    const user = req.get('user')
+    const events = JSON.stringify({
+        "key": "Global::uninstallExtension",
+        "count": 1
+    })
+    const urlbase = 'https://analytics.worldbrain.io/i'
+    const urlParams = `${urlbase}?app_key=${appKey}&device_id=${user}&events=${events}`;
+    const url = `${urlbase}?${urlParams}`
+
+    console.log(`Logging uninstall event: ${url}`)
+    await fetch(url)
+    resp.redirect(`https://worldbrain.io/uninstall/?user=${user}`)
+})
