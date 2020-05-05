d58248e80717e02849c62ed1f9dfae6ad4469abe
Oliver Sauter <oli@worldbrain.io>
2020-05-05 08:53:48 +0200

    change redirect url

--- firebase/functions/src/analytics/index.ts
+++ firebase/functions/src/analytics/index.ts
@@ -18,5 +18,5 @@ export const uninstall = functions.https.onRequest(async (req: Request, resp: ex
 
     console.log(`Logging uninstall event: ${url}`)
     await fetch(url)
-    resp.redirect(`https://worldbrain.io/uninstall/?user=${user}`)
+    resp.redirect(`https://forms.gle/t9VwLFhx9QYGDbcd6`)
 })
