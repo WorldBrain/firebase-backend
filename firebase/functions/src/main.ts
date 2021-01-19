777b51ef6a7480463456f4b93f47630706a1a20b
Vincent den Boer <vincent@youapt.eu>
2021-01-19 15:05:19 +0100

    Integrate storage hooks

--- firebase/functions/src/main.ts
+++ firebase/functions/src/main.ts
@@ -4,6 +4,7 @@ import * as functions from 'firebase-functions';
 import { activityStreamFunctions } from '@worldbrain/memex-common/lib/activity-streams/services/firebase-functions/server'
 
 import { runningInEmulator, emulatedConfig } from './constants';
+import { createFirestoreTriggers } from '@worldbrain/memex-common/lib/firebase-backend/setup';
 
 admin.initializeApp((runningInEmulator) ? emulatedConfig : undefined);
 
@@ -15,6 +16,10 @@ export { scheduledFirestoreExport } from "./backup";
 export { uninstall, uninstallLog } from "./analytics"
 export { registerBetaUserCall as registerBetaUser } from "./beta"
 export const activityStreams = activityStreamFunctions({
+    firebase: admin as any,
+    functions,
+})
+export const triggers = createFirestoreTriggers({
     firebase: admin as any,
     functions,
 })
